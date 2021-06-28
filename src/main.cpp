d41193169eed2aa6022a40bfe76f11ee128d3399
Maizo Diego <72949835+MaizoDiego@users.noreply.github.com>
2021-06-27 22:13:20 -0500

    Cambios main.cpp MaizoDiego

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include "LexicalAnalyzer.h"
 using namespace std;
 
-int main()
+int main() 
 {
-  cout << "Hello, world!";
-  return 0;
+  LexicalAnalyzer la;
+  // string exp = "a+b/(35^X-(y+3))-6";
+  //string exp = "a+b/x-y+3-6";
+  string exp = "(a+b/x)-y+sin(3-6)";
+  list<string> tokens = la.getListofTokens(exp);
+  cout << exp << endl;
+  for(auto tok: tokens)
+    cout << tok << endl;
 }
